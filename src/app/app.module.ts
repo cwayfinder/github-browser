23c23375e2121860ce2f78b0c05275525c3300bf
Taras Hupalo <taras.hupalo@gmail.com>
2019-03-14 17:07:26 +0200

    Make reducers work with AOT

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { InjectionToken, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
-import { appReducer } from './store/reducers';
+import { ActionReducerMap, StoreModule } from '@ngrx/store';
+import { AppState, repoReducers } from './store/reducers';
 import { EffectsModule } from '@ngrx/effects';
 import { AppEffects } from './store/effects';
 import { HttpClientModule } from '@angular/common/http';
 import { TrendingComponent } from './pages/trending/trending.component';
 import { DetailComponent } from './pages/detail/detail.component';
 import { SavedComponent } from './pages/saved/saved.component';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+
+export const REDUCER_TOKEN = new InjectionToken<ActionReducerMap<AppState>>('Registered Reducers');
 
 @NgModule({
   declarations: [
@@ -22,11 +26,14 @@ import { SavedComponent } from './pages/saved/saved.component';
   imports: [
     BrowserModule,
     HttpClientModule,
+    StoreModule.forRoot(REDUCER_TOKEN),
+    EffectsModule.forRoot([AppEffects]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
     AppRoutingModule,
-    StoreModule.forRoot(appReducer),
-    EffectsModule.forRoot([AppEffects])
   ],
-  providers: [],
+  providers: [
+    { provide: REDUCER_TOKEN, useValue: { repos: repoReducers } },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
