05803ce42260739ee6cdf33454359d3b179cd95c
Taras Hupalo <taras.hupalo@gmail.com>
2019-03-17 09:35:26 +0200

    Implement scroll position restoration

--- src/app/pages/trending/trending.component.ts
+++ src/app/pages/trending/trending.component.ts
@@ -3,6 +3,9 @@ import { AppState } from '../../store/reducers';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { selectRepos } from '../../store/selectors';
+import { ViewportScroller } from '@angular/common';
+import { Router, Scroll } from '@angular/router';
+import { delay, filter, switchMapTo } from 'rxjs/operators';
 
 @Component({
   selector: 'app-trending',
@@ -13,10 +16,23 @@ export class TrendingComponent implements OnInit {
 
   repos$: Observable<any[]>;
 
-  constructor(private store: Store<AppState>) { }
+  constructor(private store: Store<AppState>, private router: Router, private viewportScroller: ViewportScroller) { }
 
   ngOnInit(): void {
     this.repos$ = this.store.select(selectRepos);
+
+    this.repos$
+      .pipe(
+        switchMapTo(this.router.events),
+        filter(e => e instanceof Scroll),
+        delay(0))
+      .subscribe((e: Scroll) => {
+        if (e.position) {
+          this.viewportScroller.scrollToPosition(e.position);
+        } else {
+          this.viewportScroller.scrollToPosition([0, 0]);
+        }
+      });
   }
 
 }
